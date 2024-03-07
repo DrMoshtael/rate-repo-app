f804f152e88f93d956fdd5a3acf383a06fde23ea
Howard <howard@brightpage.tech>
2024-03-07 10:52:24 +0000

    10.7

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,25 +1,27 @@
-import { View, StyleSheet } from 'react-native';
-import Constants from 'expo-constants';
-import AppBarTab from './AppBarTab';
-import theme from '../theme';
+import { View, StyleSheet, ScrollView } from "react-native";
+import Constants from "expo-constants";
+import AppBarTab from "./AppBarTab";
+import theme from "../theme";
 
 const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight,
     backgroundColor: theme.colors.appBar,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'flex-start'
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
   },
 });
 
 const AppBar = () => {
   return (
-  <View style={styles.container}>
-    <AppBarTab path='/'>Repos</AppBarTab>
-    <AppBarTab path='/signin'>Sign in</AppBarTab>
-  </View>
-  )
+    <View style={styles.container}>
+      <ScrollView horizontal>
+        <AppBarTab path="/">Repos</AppBarTab>
+        <AppBarTab path="/signin">Sign in</AppBarTab>
+      </ScrollView>
+    </View>
+  );
 };
 
 export default AppBar;
