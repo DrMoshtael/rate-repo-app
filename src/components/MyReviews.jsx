cd0b2466cac4f4be84841553e72eb512c80ac5b2
Howard <howard@brightpage.tech>
2024-03-13 20:56:36 +0000

    10.26

--- src/components/MyReviews.jsx
+++ src/components/MyReviews.jsx
@@ -1,15 +1,65 @@
 import { useQuery } from "@apollo/client";
 import { ME } from "../graphql/queries";
-import { FlatList, View, StyleSheet } from "react-native";
+import { FlatList, View, StyleSheet, Alert, Platform } from "react-native";
 import ReviewItem from "./ReviewItem";
+import { Button } from 'react-native-paper'
+import theme from "../theme";
+import { useNavigate } from "react-router-native";
+import { useMutation } from "@apollo/client";
+import { DELETE_REVIEW } from "../graphql/mutations";
 
 const styles = StyleSheet.create({
     separator: {
       height: 10,
     },
+    buttonContainer: {
+        flexDirection: 'row',
+        backgroundColor: 'white',
+        justifyContent: 'space-evenly',
+        paddingBottom: 10
+    }
   });
 
 const ItemSeparator = () => <View style={styles.separator} />;
+
+const AReview = ({ item, refetch }) => {
+    const navigate = useNavigate();
+    const [deleteReview] = useMutation(DELETE_REVIEW);
+
+    const doDelete = async () => {
+        await deleteReview({ variables: { deleteReviewId: item.id }})
+        refetch()
+    }
+
+    const handleDelete = () => {
+        if (Platform.OS !== 'web') {
+            Alert.alert('Delete review', 'Are you sure you want to delete this review?', [
+                { 
+                    text: 'Cancel'
+                },
+                {
+                    text: 'Delete',
+                    onPress: doDelete
+                }
+            ])
+        } else {
+            if (window.confirm('Are you sure you want to delete this review?')) {
+                doDelete();
+            }
+        }
+    }
+
+    return (
+        <View>
+            <ReviewItem review={item} my_review={true}/>
+            <View style={styles.buttonContainer}>
+                <Button onPress={() => navigate(`/${item.repositoryId}`)} buttonColor={theme.colors.primary} mode='contained'>View repository</Button>
+                <Button onPress={handleDelete} buttonColor={theme.colors.error} mode='contained'>Delete review</Button>
+            </View>
+        </View>
+        
+    )
+}
 
 const MyReviews =  () => {
     const user = useQuery(ME, {
@@ -21,7 +71,7 @@ const MyReviews =  () => {
     return (
         <FlatList 
         data={reviews}
-        renderItem={({ item }) => <ReviewItem review={item} my_review={true}/>}
+        renderItem={({ item }) => <AReview item={item} refetch={user.refetch}/>}
         keyExtractor={({ id }) => id}
         ItemSeparatorComponent={ItemSeparator}
         />
